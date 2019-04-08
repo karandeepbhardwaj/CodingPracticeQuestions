c131072bbc6cdc3a0b1ba8d5aebf0aef9a2818d9
Karandeep <Karandiip@gmail.com>
2019-04-08 19:36:11 -0400

    Final Commit

--- CodingQuestions/src/Ques2.java
+++
@@ -1,8 +0,0 @@
-public class Ques2 {
-	
-	public static void main(String [] args) {
-		for(int i=0; i<5; i++) {
-			System.out.println("Welcome to java.");
-		}
-	}
-}
--- CodingQuestions/src/Ques4.java
+++
@@ -1,7 +0,0 @@
-
-public class Ques4 {
-	public static void main(String [] args) {
-		int sum = 1+2+3+4+5+6+7+8+9;
-		System.out.println(sum);
-	}
-}
